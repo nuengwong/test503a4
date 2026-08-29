a90fc76e1b50dbca183e5bf1d4ac1f6a32740fa4
Ohm <nuengwong.t@cp.eng.chula.ac.th>
2026-08-29 12:29:22 +0700

    A5

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
+
 const {getHospitals, getHospital, createHospital, updateHospital, deleteHospital} = require('../controllers/hospitals');
 
-const router = express.Router()
+const router = express.Router();
+
 
 router.route('/').get(getHospitals).post(createHospital);
 router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
 
-module.exports=router;
+module.exports = router; // export router
--- server.js
+++ server.js
@@ -1,16 +1,37 @@
+// ประกาศว่าไฟล์นี้จะมีการใช้ express ซึ่งรองรับโดย variable express
 const express = require('express');
+// ประกาศว่าไฟล์นี้จะมีการใช้ dotenv ซึ่งรองรับโดย variable dotenv
 const dotenv = require('dotenv');
-//Route files
+const connectDB = require('./config/db');
+
+// Route files
 const hospitals = require('./routes/hospitals');
+const { connect } = require('mongoose');
 
-//Load env vars
-dotenv.config({path:'./config/config.env'});
 
+// Load environment variables from .env file
+dotenv.config({ path: './config/config.env' });
+
+// connect to database
+connectDB();
+
+// สร้างตัวแปร app ที่ใช้ express ที่เรา require เข้ามา
 const app = express();
 
-//Mount routers
-app.use('/api/v1/hospitals', hospitals)
+// body parser
+app.use(express.json());
 
-const PORT = process.env.PORT || 5000
+// mount routers
+app.use('/api/v1/hospitals', hospitals); // so in hospitals.js we do not have to put /api.. anymore
 
-app.listen(PORT, console.log('Server running in', process.env.NODE_ENV, 'mode on port', PORT));
+// กำหนดให้ server รันที่ port ที่กำหนดตามในไฟล์ .env หรือถ้าไม่มีให้ใช้ port 5003
+const PORT = process.env.PORT || 5003;
+// ให้ server รันโดยฟังก์ชัน app.listen() โดยรับพอร์ตที่กำหนดและแสดงข้อความเมื่อ server รันสำเร็จ
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error : ${err.message}`);
+    // close server & exit process
+    server.close(()=>process.exit(1));
+});
